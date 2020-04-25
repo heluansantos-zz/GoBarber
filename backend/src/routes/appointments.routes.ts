836840555485c1e1cbd4f56a37b16756004b5b31
heluansantos <heluan.caetano@gmail.com>
2020-04-24 22:49:10 -0300

    Backend model de agendamento

--- backend/src/routes/appointments.routes.ts
+++ backend/src/routes/appointments.routes.ts
@@ -1,13 +1,8 @@
 import { Router } from 'express';
-import { uuid } from 'uuidv4';
 import { startOfHour, parseISO, isEqual } from 'date-fns';
+import Appointment from '../models/Appointments';
 
 const appointmentsRouter = Router();
-interface Appointment {
-  id: string;
-  provider: string;
-  date: Date;
-}
 const appointments: Appointment[] = [];
 appointmentsRouter.post('/', (request, response) => {
   const { provider, date } = request.body;
@@ -20,11 +15,7 @@ appointmentsRouter.post('/', (request, response) => {
       .status(400)
       .json({ message: 'Thie appointment is alread booked' });
   }
-  const appointment = {
-    id: uuid(),
-    provider,
-    date: parsedDate,
-  };
+  const appointment = new Appointment(provider, parsedDate);
   appointments.push(appointment);
   return response.json(appointment);
 });
