7e4e456157bb25ae04c52f4345b0eb455e652c4c
heluansantos <heluan.caetano@gmail.com>
2020-04-24 23:21:29 -0300

    Backend criando repositorios

---
+++ backend/src/repositories/AppointmentRepository.ts
@@ -0,0 +1,24 @@
+import Appointment from '../models/Appointments';
+import { isEqual } from 'date-fns';
+
+class AppointmentRepository {
+  private appointments: Appointment[];
+  constructor() {
+    this.appointments = [];
+  }
+  public findByDate(date: Date): Appointment | null {
+    const findAppintment = this.appointments.find(appointment =>
+      isEqual(date, appointment.date),
+    );
+    return findAppintment || null;
+  }
+  public create(provider: string, date: Date): Appointment {
+    const appointment = new Appointment(provider, date);
+
+    this.appointments.push(appointment);
+
+    return appointment;
+  }
+}
+
+export default AppointmentRepository;
--- backend/src/routes/appointments.routes.ts
+++ backend/src/routes/appointments.routes.ts
@@ -1,22 +1,23 @@
 import { Router } from 'express';
-import { startOfHour, parseISO, isEqual } from 'date-fns';
-import Appointment from '../models/Appointments';
+import { startOfHour, parseISO } from 'date-fns';
+import AppointmentRepository from '../repositories/AppointmentRepository';
 
 const appointmentsRouter = Router();
-const appointments: Appointment[] = [];
+const appointmentRepository = new AppointmentRepository();
 appointmentsRouter.post('/', (request, response) => {
   const { provider, date } = request.body;
   const parsedDate = startOfHour(parseISO(date));
-  const findAppintmentsInSameDate = appointments.find(appointment =>
-    isEqual(parsedDate, appointment.date),
+  const findAppintmentsInSameDate = appointmentRepository.findByDate(
+    parsedDate,
   );
   if (findAppintmentsInSameDate) {
     return response
       .status(400)
       .json({ message: 'Thie appointment is alread booked' });
   }
-  const appointment = new Appointment(provider, parsedDate);
-  appointments.push(appointment);
+
+  const appointment = appointmentRepository.create(provider, parsedDate);
+
   return response.json(appointment);
 });
 export default appointmentsRouter;
