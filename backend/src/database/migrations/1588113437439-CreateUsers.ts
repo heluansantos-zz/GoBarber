58b06548f122afdf84a2f5152a09a7bf9b39392e
heluansantos <heluan.caetano@gmail.com>
2020-04-28 21:24:40 -0300

    Backend relacionamento nos models

--- backend/src/database/migrations/1588113437439-CreateUsers.ts
+++ backend/src/database/migrations/1588113437439-CreateUsers.ts
@@ -8,7 +8,7 @@ export default class CreateUsers1588113437439 implements MigrationInterface {
         columns: [
           {
             name: 'id',
-            type: 'varchar',
+            type: 'uuid',
             isPrimary: true,
             generationStrategy: 'uuid',
             default: 'uuid_generate_v4()',
