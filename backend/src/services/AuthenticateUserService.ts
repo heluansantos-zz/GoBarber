9e2e640c09c1954a539275337aa37bada29da672
heluansantos <heluan.caetano@gmail.com>
2020-04-28 23:39:44 -0300

    Backend validando credenciais

---
+++ backend/src/services/AuthenticateUserService.ts
@@ -0,0 +1,35 @@
+import { getRepository } from 'typeorm';
+import User from '../models/User';
+import { compare } from 'bcryptjs';
+
+interface Request {
+  email: string;
+  password: string;
+}
+
+interface Response {
+  user: User;
+}
+
+class AuthenticateUserService {
+  public async execute({ email, password }: Request): Promise<Response> {
+    const usersRepository = getRepository(User);
+
+    const user = await usersRepository.findOne({ where: { email } });
+    if (!user) {
+      throw new Error('Incorrect email/password combination.');
+    }
+    //user.password - Senha criptografada
+    //password - Senha não criptografada
+    const passwordMatched = await compare(password, user.password);
+    if (!passwordMatched) {
+      throw new Error('Incorrect email/password combination.');
+    }
+    //Usuário autenticado
+    return {
+      user,
+    };
+  }
+}
+
+export default AuthenticateUserService;
