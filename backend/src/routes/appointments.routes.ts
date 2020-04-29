8ea65bfde83fbdd5bec60f6660e2d5c68b1b0a83
heluansantos <heluan.caetano@gmail.com>
2020-04-29 16:28:14 -0300

    Backend correçao de erro

--- backend/src/routes/appointments.routes.ts
+++ backend/src/routes/appointments.routes.ts
@@ -9,9 +9,9 @@ const appointmentsRouter = Router();
 
 appointmentsRouter.use(ensureAuthenticated);
 
-appointmentsRouter.get('/', (request, response) => {
+appointmentsRouter.get('/', async (request, response) => {
   const appointmentsRepository = getCustomRepository(AppointmentsRepository);
-  const appointments = appointmentsRepository.find();
+  const appointments = await appointmentsRepository.find();
   return response.json(appointments);
 });
 
